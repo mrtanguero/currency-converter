85f8b6d8f1c3686eb14a6a842ff49b82501cf330
Ivan Jovanović <mrtanguero@gmail.com>
2021-01-10 14:33:04 +0100

    Male korekcije u vezi sa UI

--- src/components/Message.js
+++ src/components/Message.js
@@ -12,22 +12,23 @@ export default function Message({ state }) {
   } else if (Number.isNaN(state.result)) {
     return (
       <div className="message red">
-        <h2>Samo validni brojevi mogu biti konvertovani.</h2>
+        <h2>Invalid amount.</h2>
       </div>
     );
   } else if (state.result === "error") {
     return (
       <div className="message red">
-        <h2>Problem sa API pozivom.</h2>
+        <h2>API call failed. Check console for details.</h2>
       </div>
     );
   } else {
     return (
       <div className="message">
         <h1>
-          {state.amount}
-          {state.from} = {state.result}
-          {state.to}
+          <span className="amount">{state.amount}</span>
+          <span className="currency">{state.from}</span> ={" "}
+          <span className="amount">{state.result}</span>
+          <span className="currency">{state.to}</span>
         </h1>
       </div>
     );
